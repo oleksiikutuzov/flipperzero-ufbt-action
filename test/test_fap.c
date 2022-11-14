c5e102098778e9be7d2ced10e3c60eb61dc3b99a
Oleksii Kutuzov <8535871+oleksiikutuzov@users.noreply.github.com>
2022-11-14 13:47:54 +0100

    Format file

--- test/test_fap.c
+++ test/test_fap.c
@@ -1,9 +1,9 @@
 #include <furi.h>
 
-int32_t test_fap_app(void *p) {
-  UNUSED(p);
-  FURI_LOG_I("TEST", "Hello world");
-  FURI_LOG_I("TEST", "I'm test_fap!");
+int32_t test_fap_app(void* p) {
+    UNUSED(p);
+    FURI_LOG_I("TEST", "Hello world");
+    FURI_LOG_I("TEST", "I'm test_fap!");
 
-  return 0;
+    return 0;
 }
