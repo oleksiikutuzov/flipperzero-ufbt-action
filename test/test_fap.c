d1bddbdbcaf34268642898abfe361d0e3a582564
Oleksii Kutuzov <8535871+oleksiikutuzov@users.noreply.github.com>
2022-11-14 19:52:49 +0100

    Break test app

--- test/test_fap.c
+++ test/test_fap.c
@@ -3,7 +3,8 @@
 int32_t test_fap_app(void* p) {
     UNUSED(p);
     FURI_LOG_I("TEST", "Hello world");
-    FURI_LOG_I("TEST", "I'm test_fap!");
+    FURI_LOG_I("TEST",
+	"I'm test_fap!")
 
     return 0;
 }
